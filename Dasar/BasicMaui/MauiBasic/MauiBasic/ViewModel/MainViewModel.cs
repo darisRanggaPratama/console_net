2ebc86a8d1c9a23ee8c56aac304766e6b46f76f7
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-02-05 21:39:19 +0700

    initial commit

--- Dasar/BasicMaui/MauiBasic/MauiBasic/ViewModel/MainViewModel.cs
+++ Dasar/BasicMaui/MauiBasic/MauiBasic/ViewModel/MainViewModel.cs
@@ -5,44 +5,53 @@ using System.Collections.ObjectModel;
 
 namespace MauiBasic.ViewModel;
 
-	public partial class MainViewModel : ObservableObject
+public partial class MainViewModel : ObservableObject
+{
+	IConnectivity connectivity;
+
+	public MainViewModel(IConnectivity connectivity)
 	{
-		public MainViewModel()
+		Items = new ObservableCollection<string>();
+		this.connectivity= connectivity;
+	}
+
+	[ObservableProperty]
+	ObservableCollection<string> items;
+
+	[ObservableProperty] 
+	string text;
+
+	[RelayCommand]
+	async Task Add()
+	{
+		if (string.IsNullOrWhiteSpace(Text))
+			return;
+
+		if (connectivity.NetworkAccess != NetworkAccess.Internet)
 		{
-			Items = new ObservableCollection<string>();
+			await Shell.Current.DisplayAlert("Oh No", "No Internet", "Ok");
+			return;
 		}
 
-		[ObservableProperty]
-		ObservableCollection<string> items;
+		Items.Add(Text);
 
-		[ObservableProperty]
-		string text;
+		// Add our item
+		Text = string.Empty;
+	}
 
-		[RelayCommand]
-		void Add()
+	[RelayCommand]
+	void Delete(string s)
+	{
+		if (Items.Contains(s))
 		{
-			if (string.IsNullOrWhiteSpace(Text))
-				return;
-
-			Items.Add(Text);
-
-			// Add our item
-			Text = string.Empty;
-		}
-
-		[RelayCommand]
-		void Delete(string s)
-		{
-			if (Items.Contains(s))
-			{
-				Items.Remove(s);
-			}
-		}
-
-		[RelayCommand]
-		async Task Tap(string s)
-		{
-			await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+			Items.Remove(s);
 		}
 	}
+
+	[RelayCommand]
+	async Task Tap(string s)
+	{
+		await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+	}
+}
 
