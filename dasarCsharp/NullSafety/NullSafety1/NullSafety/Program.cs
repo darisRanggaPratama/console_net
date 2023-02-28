7300f5cf42bd183e1e5de68d635b64eaa146b9cf
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-02-28 23:05:59 +0700

    initial commit

---
+++ dasarCsharp/NullSafety/NullSafety1/NullSafety/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Nullable
+{
+    class NullSafety
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jhon Doe";
+            string? nullName = null;
+
+            // 1. Cek nilai null
+            if (nullName != null)
+            {
+                Console.WriteLine($"Name: {nullName}");
+            }
+            else
+            {
+                Console.WriteLine("Name is Null");
+            }
+
+            // 2. Cek Null (null coalescing)
+            string defaultOrName = nullName ?? "DefaultName";
+            Console.WriteLine($"Name: {defaultOrName}");
+            
+            // 3. Menggunakan null propagation operator untuk menghindari NullReferenceException
+            int? panjang = nullName?.Length;
+            Console.WriteLine($"Name Length: {(panjang.HasValue ? panjang.ToString() : "null")}");
+
+
+        }
+    }
+}
